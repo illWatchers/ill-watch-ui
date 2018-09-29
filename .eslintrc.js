63e5624c510d8716dbb85c8b4b4014be04a7aa89
Dmytro <dmytro.ilhanaiev@gmail.com>
2018-09-29 23:17:12 +0300

    Setup illwatchers eslint common config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,6 @@
 module.exports = {
     "extends": [
-        "airbnb",
-        "react-app"
-    ],
-    "rules": {
-        "react/jsx-filename-extension": [1, {"extensions": [".js"]}],
-        "no-extra-semi": "error",
-        "semi": ["error", "never"],
-        "eol-last": ["error", "never"],
-    }
+        "react-app",
+        "@illwatchers"
+    ]
 }
