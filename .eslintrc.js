5724140fcabfe833e2a0e3ca16545daef6f26a45
Dmytro <dmytro.ilhanaiev@gmail.com>
2018-10-01 04:31:13 +0300

    According to updated common eslin-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
     "extends": [
+        "airbnb",
         "react-app",
         "@illwatchers"
-    ]
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [1, {"extensions": [".js"]}]
+    }
 }
