bddcfd2cd0b30696252f82f576470745adae5cb6
spahuta <spahuta@redhat.com>
2018-10-23 19:08:24 +0200

    Minimal gql setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
 
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import client from './graphql/apollo'
 
 ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
 ), document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
